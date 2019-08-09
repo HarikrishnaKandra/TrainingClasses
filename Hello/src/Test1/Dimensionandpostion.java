15f77b64742c37213dd731d4eed1b6f293fa749e
Admin <Admin@DESKTOP-BSLRFLR>
2019-08-09 20:05:38 +0530

    Updated

--- Hello/src/Test1/Dimensionandpostion.java
+++ Hello/src/Test1/Dimensionandpostion.java
@@ -40,7 +40,7 @@ public static void main(String[] args) throws InterruptedException
 	    System.out.println("Y-Value" + Y);
 	    
 	    Thread.sleep(5000);
-	    //Setting Position
+	    //Setting Position hicvfghyfuyfgcvuy
 	    
 	    Point P = new Point(20,80);
 	    driver.manage().window().setPosition(P);
